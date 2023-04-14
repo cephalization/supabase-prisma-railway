97944fd978affebdd2d0ba45616ead7c1bbf5775
Anthony Powell <anthonypowell@hey.com>
2023-04-14 14:15:11 -0400

    Turn script into long running script

--- script.ts
+++ script.ts
@@ -1,28 +1,46 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+async function poll() {
+  return new Promise(async (resolve) => {
+    console.log("Fetching posts...");
+    const posts = await prisma.post.findMany({
+      where: {
+        published: true,
+      },
+    });
+
+    console.log(posts);
+
+    setTimeout(() => resolve(poll()), 10000);
+    console.log(`Fetched at: ${new Date().getSeconds()}s`);
+    console.log("Polling for new posts in 10 seconds...");
+  });
+}
 
 async function main() {
   // Retrieve all published posts
   const allPosts = await prisma.post.findMany({
     where: { published: true },
-  })
-  console.log('Retrieved all published posts: ', allPosts)
+  });
+  console.log("Retrieved all published posts: ", allPosts);
 
   // Create a new post (written by an already existing user with email alice@prisma.io)
   const newPost = await prisma.post.create({
     data: {
       title: "Join us for another episode of What's new in Prisma",
-      content: 'https://youtube.com/playlist?list=PLn2e1F9Rfr6l1B9RP0A9NdX7i7QIWfBa7',
+      content:
+        "https://youtube.com/playlist?list=PLn2e1F9Rfr6l1B9RP0A9NdX7i7QIWfBa7",
       published: false,
       author: {
         connect: {
-          email: 'alice@prisma.io',
+          email: "alice@prisma.io",
         },
       },
     },
-  })
-  console.log('Created a new post:', newPost)
+  });
+  console.log("Created a new post:", newPost);
 
   // Publish the new post
   const updatedPost = await prisma.post.update({
@@ -32,26 +50,37 @@ async function main() {
     data: {
       published: true,
     },
-  })
-  console.log(`Published the newly created post: `, updatedPost)
+  });
+  console.log(`Published the newly created post: `, updatedPost);
 
   // Retrieve all posts by user with email alice@prisma.io
   const postsByUser = await prisma.user
     .findUnique({
       where: {
-        email: 'alice@prisma.io',
+        email: "alice@prisma.io",
       },
     })
-    .posts()
-  console.log(`Retrieved all posts from a specific user: `, postsByUser)
+    .posts();
+  console.log(`Retrieved all posts from a specific user: `, postsByUser);
+
+  console.log("Polling for new posts...");
+  await poll();
 }
+
+async function cleanup() {
+  await prisma.$disconnect();
+  process.exit(1);
+}
+
+process.on("SIGINT", cleanup);
+process.on("SIGTERM", cleanup);
+process.on("SIGKILL", cleanup);
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await cleanup();
+  });
